36069fc38285fde53ce74c2da3d62bd843b7cbda
Shashank Chauhan <shashank.chauhan.ec.2018@miet.ac.in>
2023-08-28 14:32:10 +0530

    Add project files.

---
+++ First1 Web App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace First1_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
